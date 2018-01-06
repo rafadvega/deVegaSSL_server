6172a9ccb1795677f18919f6712f3887a47c5bf5
Rafa de Vega <rafadevega@gmail.com>
2018-01-06 18:15:52 +0100

    Server base added

---
+++ base64.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   base64.h
+ * Author: rdevega
+ *
+ * Created on 25 de noviembre de 2017, 17:43
+ */
+
+#ifndef BASE64_H
+#define BASE64_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BASE64_H */
+
